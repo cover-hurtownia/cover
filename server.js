8b39b126516ffa3ee1e4f8db4fe8cf92f7361da5
Bittersweet Shimmer <bttrswt@protonmail.com>
2020-12-22 19:29:05 +0100

    add users db table, login & register API endpoints

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
-import path from "path";
-
-import express from "express";
+import express, { request, response } from "express";
 import session from "express-session";
 import bodyParser from "body-parser";
 
@@ -9,6 +7,7 @@ import connectSessionKnex from "connect-session-knex";
 import knexFile from "./knexfile.js";
 const KnexSessionStore = connectSessionKnex(session);
 
+import bcrypt from "bcryptjs";
 
 
 const SESSION_COOKIE_NAME = "session";
@@ -27,7 +26,7 @@ app.use(session({
     name: SESSION_COOKIE_NAME,
     rolling: true,
     resave: true,
-    saveUninitialized: true,
+    saveUninitialized: false,
     cookie: {
         sameSite: true,
         httpOnly: true,
@@ -44,6 +43,136 @@ app.post("/api/echo", (request, response) => {
         message: "Hello from /api/echo.",
         data: request.body
     });
+});
+
+app.post("/api/login", async (request, response) => {
+    const username = request.body.username ?? "";
+    const password = request.body.password ?? "";
+
+    try {
+        const usersInDatabaseQuery = db('users').where({ username });
+        const usersInDatabase = await usersInDatabaseQuery.catch(error => {
+            console.error(`/api/register: database error: ${usersInDatabaseQuery.toString()}: ${error}`);
+            throw "database error";
+        });
+
+        if (usersInDatabase.length != 1) {
+            throw "invalid username or password";
+        }
+
+        const user = usersInDatabase[0];
+
+        const passwordMatches = await bcrypt.compare(password, user.password_hash).catch(_ => {
+            console.error("/api/login: hash comparison error");
+            throw "internal error";
+        });
+
+        if (!passwordMatches) {
+            throw "invalid username or password";
+        }
+
+        request.session.username = username;
+
+        response.status(200);
+        response.send({
+            status: "ok"
+        });
+    }
+    catch (error) {
+        response.status(400)
+        response.send({
+            status: "error",
+            error
+        });
+    }
+});
+
+app.post("/api/register", async (request, response) => {
+    const username = request.body.username ?? "";
+    const password = request.body.password ?? "";
+
+    try {
+        if (!Array.from(username).every(character => {
+            const code = character.charCodeAt(0);
+    
+            const isNumeric = code > 47 && code < 58;
+            const isUpperAlpha = code > 64 && code < 91;
+            const isLowerAlpha = code > 96 && code < 123;
+            const isUnderscore = character === "_";
+    
+            return isNumeric || isUpperAlpha || isLowerAlpha || isUnderscore;
+        })) {
+            throw "username can't contain characters other than 0-9/a-z/A-Z/_";
+        };
+
+        if (username.length < 3) {
+            throw "username must consist of at least 3 characters";
+        }
+        
+        if (password.length === 0) {
+            throw "password can't be empty";
+        };
+
+        const salt = await bcrypt.genSalt().catch(_ => {
+            console.error("/api/register: salt generation error");
+            throw "internal error"
+        });
+        const passwordHash = await bcrypt.hash(password, salt).catch(_ => {
+            console.error("/api/register: hashing error");
+            throw "internal error";
+        });
+
+        const usersInDatabaseQuery = db('users').where({ username });
+        const usersInDatabase = await usersInDatabaseQuery.catch(error => {
+            console.error(`/api/register: database error: ${usersInDatabaseQuery.toString()}: ${error}`);
+            throw "database error";
+        });
+
+        if (usersInDatabase.length != 0) { throw "username already exists" }
+
+        const insertUserQuery = db('users').insert({ username, password_hash: passwordHash });
+
+        await insertUserQuery.catch(error => {
+            console.error(`/api/register: database error: ${insertUserQuery.toString()}: ${error}`);
+            throw "database error";
+        });
+
+        request.session.username = username;
+
+        response.status(200);
+        response.send({
+            status: "ok"
+        });
+    }
+    catch (error) {
+        response.status(400)
+        response.send({
+            status: "error",
+            error
+        });
+    }
+});
+
+app.post("/api/logout", async (request, response) => {
+    try {
+        if (!request.session.hasOwnProperty("username")) {
+            throw "already logged out";
+        };
+
+        request.session.destroy();
+
+        response.status(200);
+        response.send({
+            status: "ok"
+        });
+    }
+    catch (error) {
+        response.status(400)
+        response.send({
+            status: "error",
+            error
+        });
+    }
 });
 
 export default app;
