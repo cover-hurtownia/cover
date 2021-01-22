54cd848411cf7b3391424ec80fd88c6bf5592bd5
Michał Mazur 11 <kubala6666@gmail.com>
2021-01-22 15:40:26 +0100

    fix header footer

--- www/js/main.js
+++ www/js/main.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-
     const defTheme = document.body.dataset;
     document.cookie = `theme = ${defTheme.theme}`;
     const buttons = document.querySelectorAll(".btn");
+
     const changeTheme = ({value}) => {
         switch (value){
             case "light":
@@ -13,26 +13,39 @@ document.addEventListener("DOMContentLoaded", () => {
                 break;
             case "contrast":
                 defTheme.theme = "contrast";
-
                 break;
             case "font-add":
+                
                 break;
             case "font-subtract":
-                console.log()
+                
                 break;
             default:
                 console.error("Header button error");
         };
-    }
-    for(const button of buttons){
+    };
+    buttons.forEach(button => {
         button.addEventListener('click', _ => {
             changeTheme(button);
         });
-    };
+    })
 
     const burger = document.querySelector(".navbar-burger");
     const navbarMenu = document.querySelector("#navbar-burger-elements");
     burger.addEventListener("click", (e) => {
         navbarMenu.classList.toggle("is-active");
-    })
+    });
+
+    const toTopButton = document.querySelector(".btn-to-top");
+    if(toTopButton !== null){toTopButton.addEventListener("click", _ => {
+        window.scrollTo(0, 0);
+    });}
+
+    document.addEventListener("scroll", _ => {
+        if(this.scrollY > 200){
+            toTopButton.classList.add("active");
+        } else {
+            toTopButton.classList.remove("active");
+        }
+    });
 });
