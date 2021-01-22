ea4184b5ca1ca2f04238e817f8b96ca99119a833
Bittersweet Shimmer <bttrswt@protonmail.com>
2021-01-22 19:56:04 +0100

    add themes as scss, refactor

--- www/js/main.js
+++ www/js/main.js
@@ -1,38 +1,79 @@
-document.addEventListener("DOMContentLoaded", () => {
+const cookies = Object.fromEntries(document.cookie.split(";").map(_ => _.split("=")));
 
-    const defTheme = document.body.dataset;
-    document.cookie = `theme = ${defTheme.theme}`;
-    const buttons = document.querySelectorAll(".btn");
-    const changeTheme = ({value}) => {
-        switch (value){
-            case "light":
-                defTheme.theme = "light";
-                break;
-            case "dark":
-                defTheme.theme = "dark";
-                break;
-            case "contrast":
-                defTheme.theme = "contrast";
+/*
+    themes
+*/
 
-                break;
-            case "font-add":
-                break;
-            case "font-subtract":
-                console.log()
-                break;
-            default:
-                console.error("Header button error");
-        };
+const validThemes = ["light", "dark", "contrast"];
+const isThemeValid = theme => validThemes.includes(theme);
+const defaultTheme = "light";
+
+const theme = isThemeValid(cookies.theme) ? cookies.theme : defaultTheme;
+const themeLinkElement = document.getElementById("bulma-theme");
+
+const themeButtons = document.querySelectorAll(".theme-button");
+const changeTheme = theme => {
+    if (isThemeValid(theme)) {
+        document.cookie = `theme=${theme}`;
+        themeLinkElement.href = `/css/bulma-${theme}.css`;
     }
-    for(const button of buttons){
-        button.addEventListener('click', _ => {
-            changeTheme(button);
-        });
-    };
+};
 
-    const burger = document.querySelector(".navbar-burger");
-    const navbarMenu = document.querySelector("#navbar-burger-elements");
-    burger.addEventListener("click", (e) => {
-        navbarMenu.classList.toggle("is-active");
-    })
+changeTheme(theme);
+
+for (const button of themeButtons) {
+    button.addEventListener("click", _ => changeTheme(button.value));
+};
+
+/*
+    font size
+*/
+
+const validFontSizes = ["normal", "large"];
+const isFontSizeValid = font => validFontSizes.includes(font);
+const defaultFontSize = "normal";
+
+const fontSize = isFontSizeValid(cookies.font_size) ? cookies.font_size : defaultFontSize;
+
+const fontSizeButtons = document.querySelectorAll(".font-size-button");
+const changeFontSize = font => {
+    if (isFontSizeValid(font)) {
+        document.cookie = `font_size=${font}`;
+        document.body.dataset.fontSize = font;
+    }
+};
+
+changeFontSize(fontSize);
+
+for (const button of fontSizeButtons) {
+    button.addEventListener("click", _ => changeFontSize(button.value));
+};
+
+/*
+    navbar burger
+*/
+
+const burger = document.querySelector(".navbar-burger");
+const navbarMenu = document.querySelector("#navbar-burger-elements");
+
+burger.addEventListener("click", _ => {
+    navbarMenu.classList.toggle("is-active");
+});
+
+/*
+    btn-to-top
+*/
+
+const toTopButton = document.querySelector("#btn-to-top");
+
+toTopButton.addEventListener("click", _ => {
+    window.scrollTo(0, 0);
+});
+
+document.addEventListener("scroll", _ => {
+    if (window.scrollY > 200){
+        toTopButton.classList.add("active");
+    } else {
+        toTopButton.classList.remove("active");
+    }
 });
